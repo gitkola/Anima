0b7aaa45abfd8ff237316b0d7ef79d14c7e8181b
LavrovNick <lavrovmykola@gmail.com>
2024-03-17 16:42:01 +0200

    Initial Commit

---
+++ Anima/AnimaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimaApp.swift
+//  Anima
+//
+//  Created by Nick Lavrov on 17.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
